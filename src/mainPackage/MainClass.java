10d864fca8fd69ac1db68575eb14ae34c6b6e4eb
ANAS MAKOUAR <118914175+makouaranas@users.noreply.github.com>
2025-01-14 14:40:37 +0100

    add remove function
    i add some new setting on the search function and i add the remove function to the studentdatabase class

--- src/mainPackage/MainClass.java
+++ src/mainPackage/MainClass.java
@@ -1,11 +1,15 @@
 package mainPackage;
 
-
+import java.sql.SQLException;
 
 
 public class MainClass {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
+			StudentsDatabase data = new StudentsDatabase("students");
+			//data.modifierEtudiant("P142063053", new Students("P142063053","anas","ezz",new Date(),"hi@gmail.com"));
+			
+			data.supprimerEtudiant("Y681871");
 			
 	}
 

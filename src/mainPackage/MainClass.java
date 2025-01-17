cca4ed4117d4a4810df4407c63aeb3fb9b01fb33
ANAS MAKOUAR <118914175+makouaranas@users.noreply.github.com>
2025-01-17 23:44:20 +0100

    0

--- src/mainPackage/MainClass.java
+++ src/mainPackage/MainClass.java
@@ -5,10 +5,10 @@ import java.sql.SQLException;
 public class MainClass {
 	
 	public static void main(String[] args) throws SQLException {
-			StudentsDatabase data = new StudentsDatabase("students");
-		
-			InterfaceGraphique gui = new InterfaceGraphique();
-			gui.getAccueil();
+//			StudentsDatabase data = new StudentsDatabase("students");
+//		
+//			InterfaceGraphique gui = new InterfaceGraphique();
+//			gui.getAccueil();
 			
 	}
 }
